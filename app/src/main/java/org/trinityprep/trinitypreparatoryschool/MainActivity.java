41938daaf350781a46b8f9c43f3e7817e819e9a0
Dominic Martinez <martinezd19@trinityprep.org>
2016-05-12 22:56:38 -0400

    Base version
    Adds correct tabs/icons/labels

--- app/src/main/java/org/trinityprep/trinitypreparatoryschool/MainActivity.java
+++ app/src/main/java/org/trinityprep/trinitypreparatoryschool/MainActivity.java
@@ -72,13 +72,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_schedule) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_news) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_grille) {
+
+        } else if (id == R.id.nav_exam_schedule) {
+
+        } else if (id == R.id.nav_settings) {
 
         }
 
